90a1849ea2307c2d8f394bc1fb922fc0c1d92f75
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-04-27 21:40:12 +0300

    Implements test classes

--- AutoMapper/AutoMapper.Tests.TestData/Employee.cs
+++ AutoMapper/AutoMapper.Tests.TestData/Employee.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace AutoMapper.Tests.TestData
 {
-    class Employee
+    public class Employee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Sex { get; set; }
     }
 }
--- AutoMapper/AutoMapper.Tests.TestData/EmployeeDto.cs
+++ AutoMapper/AutoMapper.Tests.TestData/EmployeeDto.cs
@@ -4,5 +4,8 @@ namespace AutoMapper.Tests.TestData
 {
     public class EmployeeDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
